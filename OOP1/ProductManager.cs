ffc24066c2e28822e34c26b81ab9fa57aad06bf4
Furkan BASKAN <frknbskn@gmail.com>
2021-01-23 17:17:00 +0300

    23 Jan. 21'

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        //encapsulation
+        public void Add(Product product)
+        {
+            Console.WriteLine(product.ProductName + " eklendi.");
+
+        }
+
+        public void update(Product product)
+        {
+            Console.WriteLine(product.ProductName + " güncellendi.");
+        }
+
+        
+    }
+}
