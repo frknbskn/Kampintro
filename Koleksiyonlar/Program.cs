e2e79ab76270c5060163594890cb3a92569e5338
Furkan BAŞKAN <frknbskn@gmail.com>
2022-03-09 20:39:37 +0300

    Update Program.cs

--- Koleksiyonlar/Program.cs
+++ Koleksiyonlar/Program.cs
@@ -7,19 +7,19 @@ namespace Koleksiyonlar
     {
         static void Main(string[] args)
         {
-            //string[] isimler = new string[] { "Furkan", "Neslihan", "Gizem", "Eymen" };
+            //string[] isimler = new string[] { "Furkan", "Neslihan", "Eymen" };
             //Console.WriteLine(isimler[0]);
             //Console.WriteLine(isimler[1]);
             //Console.WriteLine(isimler[2]);
-            //Console.WriteLine(isimler[3]);
+            
 
 
 
-            List<string> isimler2 = new List<string> { "Furkan", "Neslihan", "Gizem", "Eymen" };
+            List<string> isimler2 = new List<string> { "Furkan", "Neslihan",  "Eymen" };
             Console.WriteLine(isimler2[0]);
             Console.WriteLine(isimler2[1]);
             Console.WriteLine(isimler2[2]);
-            Console.WriteLine(isimler2[3]);
+            
             isimler2.Add("Başkan");
             Console.WriteLine(isimler2[4]);
             Console.WriteLine(isimler2[0]);
